f44203524e7f44da27741e0b74a24b736054ae9b
joao-devmaster <joaovitorsantossouza7@gmail.com>
2020-05-25 17:17:06 -0300

    primeiroPOO

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System;
+
+namespace TesteClass
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // atributos
+        carro Chevrolet = new carro();
+        Chevrolet.corrida = "Velocidade";
+
+        Console.WriteLine(Chevrolet.marca);
+
+        Console.WriteLine(Chevrolet.corrida);
+
+        Console.WriteLine(Chevrolet.correr());
+    
+
+        
+
+       
+
+        }
+
+    
+       
+        
+        
+    }
+
+}
+
+
---
+++ carros.cs
@@ -0,0 +1,19 @@
+namespace TesteClass
+{
+public class carro
+ {
+      // classe
+      public string marca = "Chevrolet";
+      public int ano = 2020;
+      public string corrida;
+
+      public string correr()
+      {
+          return "o carro saiu para a corrida";
+      }
+    }
+
+
+
+ }
+        
